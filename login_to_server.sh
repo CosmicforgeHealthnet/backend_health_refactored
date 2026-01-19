743d76ef0d5846028ded67827a20533279f902b6
Mgenius3 <benmos16@gmail.com>
2026-01-19 14:06:46 +0100

    Remove connect_db.sh and login_to_server.sh from tracking

--- login_to_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Server credentials
-HOST="72.61.20.94"
-USER="root"
-PASSWORD="CForgehealthnetne&t2025"
-
-# Connect via SSH using sshpass
-sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no "$USER@$HOST"
