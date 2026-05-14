954b6a669180beec308bcd3ca8d4edcd84b34be6
Triumph <triumphgroup123@gmail.com>
2026-05-14 21:26:46 +0100

    feat: add script to seed verified test patient, doctor, and pharmacy accounts

--- scripts/seed-test-accounts.js
+++ scripts/seed-test-accounts.js
@@ -61,8 +61,8 @@ async function run() {
     `, [HASHES.patient]);
 
     await client.query(`
-      INSERT INTO patient_profiles ("userId", "profileType", gender, nationality)
-      VALUES ($1, 'individual', 'male', 'Nigerian')
+      INSERT INTO patient_profiles ("userId", "profileType", gender, nationality, "dateOfBirth")
+      VALUES ($1, 'individual', 'male', 'Nigerian', '1990-01-01')
     `, [patientUser.id]);
 
     await client.query(`
