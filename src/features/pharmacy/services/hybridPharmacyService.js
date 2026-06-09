99f82bb430b5f1062d2ed9240478bb204facb8f9
Triumph <triumphgroup123@gmail.com>
2026-06-09 18:15:03 +0100

    feat: implement HybridPharmacyService to manage vendor mode for pharmacy profiles

--- src/features/pharmacy/services/hybridPharmacyService.js
+++ src/features/pharmacy/services/hybridPharmacyService.js
@@ -1,4 +1,4 @@
-const { AppDataSource } = require("../../../config/database");
+const AppDataSource = require("../../../config/database");
 
 const VALID_VENDOR_CATEGORIES = [
     "health_wellness",
